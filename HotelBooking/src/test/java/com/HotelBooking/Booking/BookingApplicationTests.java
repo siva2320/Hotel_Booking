b5eeef62135e5b5b201cf6e919891589773f8097
siva2320 <sivagnanam2320@gmail.com>
2025-09-10 13:30:51 +0530

    Add files via upload

---
+++ HotelBooking/src/test/java/com/HotelBooking/Booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotelBooking.Booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
